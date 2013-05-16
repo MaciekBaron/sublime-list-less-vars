abe9996dc9a5bf7b4ffbaed7c000390df86b1578
Maciek Baron <thebezet@gmail.com>
2013-05-16 15:00:59 +0100

    Improving regex, displaying value

--- lessvariables.py
+++ lessvariables.py
@@ -2,13 +2,15 @@ import sublime, sublime_plugin
 
 class ListLessVariables(sublime_plugin.TextCommand):
     def run(self, edit):
-    	fn = self.view.file_name().encode("utf_8")
-    	if not fn.endswith(".less"):
-    		return
+        fn = self.view.file_name().encode("utf_8")
+        if not fn.endswith(".less"):
+            return
         self.variables = []
-        self.view.find_all("(@(?!((import|media|page|keyframes|font-face)(\s|\{)|(-(webkit|moz|o|ms)-)))[^\s\\];:\{]*)", 0, "$1", self.variables)
+        self.view.find_all("(@[^\s\\]*;:,\{]*): (.*);", 0, "$1|$2", self.variables)
         self.variables = list(set(self.variables))
         self.variables.sort()
+        for i, val in enumerate(self.variables):
+            self.variables[i] = val.split("|")
         self.view.window().show_quick_panel(self.variables, self.insert_variable, sublime.MONOSPACE_FONT)
 
     def insert_variable(self, choice):
