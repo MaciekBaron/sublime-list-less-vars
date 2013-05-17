c763107e5df2a79415cc666661ae16d23fe7a82e
Maciek Baron <thebezet@gmail.com>
2013-05-17 12:44:12 +0100

    Adding @import support

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Simple Sublime 2 plugin for listing LESS variables used in a file.
 The default hotkey is <kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>L</kbd>.
 
 It displays a list of LESS variables used in your current file allowing you to insert a selected one 
-directly into your code.
+directly into your code. It also supports `@import`ed LESS files so you can use variables defined in 
+external files.
 
 Note that the plugin automatically ignores anything which looks like a vendor prefixed statement (e.g. 
 `@-webkit-keyframes`) and reserved words (e.g. `@media`, `@import` etc.)
--- lessvariables.py
+++ lessvariables.py
@@ -1,16 +1,38 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os, re
 
 class ListLessVariables(sublime_plugin.TextCommand):
     def run(self, edit):
         fn = self.view.file_name().encode("utf_8")
         if not fn.endswith(".less"):
             return
+            
+        # Handle imports
+        imports = []
+        imported_vars = []
+        self.view.find_all("@import \"(.*)\";", 0, "/$1.less", imports)
+
+        file_dir = os.path.dirname(fn)
+
+        for i, val in enumerate(imports):
+            try:
+                f = open(file_dir + val, 'r')
+                contents = f.read()
+
+                m = re.findall("(@[^\s\\]]*): *(.*);", contents)
+                imported_vars = imported_vars + m
+            except:
+                print "Could not load file " + val
+
+        # Convert a list of tuples to a list of lists
+        imported_vars = [list(item) for item in imported_vars]
+
         self.variables = []
         self.view.find_all("(@[^\s\\]]*): *(.*);", 0, "$1|$2", self.variables)
         self.variables = list(set(self.variables))
-        self.variables.sort()
         for i, val in enumerate(self.variables):
             self.variables[i] = val.split("|")
+        self.variables = imported_vars + self.variables
+        self.variables.sort()
         self.view.window().show_quick_panel(self.variables, self.insert_variable, sublime.MONOSPACE_FONT)
 
     def insert_variable(self, choice):
